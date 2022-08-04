66fc5825e3dea99a544d39f106ea927065fbbb96
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-08-04 18:16:18 +0600

    Update OurPrograms.js

--- pages/components/comps/OurPrograms.js
+++ pages/components/comps/OurPrograms.js
@@ -8,7 +8,7 @@ const OurPrograms = () => {
     <section className="min-h-full py-20">
       <div className="h-full grid grid-cols-1 content-center">
         <div className="max-w-2xl mx-auto py-5">
-          <h2 className="text-[24px] text-slate-600 font-bold sm:mt-4 lg:text-[45px] text-center px-5 uppercase tracking-wider">
+          <h2 className="text-[24px] text-slate-600 font-bold lg:text-[45px] text-center px-5 uppercase tracking-wider">
             Our Programs
           </h2>
         </div>
