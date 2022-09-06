db296090f75c203dcbe17d54dce3f31fc1870e7a
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-06 17:47:03 +0600

    update

--- pages/pages/service/DetailsService.js
+++ pages/pages/service/DetailsService.js
@@ -1,5 +1,6 @@
+import Image from "next/image";
 import React from "react";
-import Feature from "./Feature";
+// import Feature from "./Feature";
 
 const DetailsService = () => {
   const data = [
@@ -25,8 +26,40 @@ const DetailsService = () => {
   return (
     <>
       {data.length > 0 &&
-        data.map((data, index) => (
-          <Feature key={index.toString()} data={data} />
+        data.map((item) => (
+          // <Feature key={index.toString()} data={data} />
+          <div
+            key={item.id}
+            className="max-w-7xl mx-auto text-black px-4 lg:px-24 grid gap-y-2 py-10 lg:py-10"
+          >
+            <div className="grid grid-cols-1 md:grid-cols-2 px-6 gap-x-6 items-center">
+              <div
+                className={
+                  item.id % 2 === 0
+                    ? "order-1 flex items-center justify-center"
+                    : "order-2 flex items-center justify-center"
+                }
+              >
+                <Image
+                  src={item.img}
+                  width={350}
+                  height={350}
+                  className="rounded p-5 lg:p-12"
+                  alt=""
+                />
+              </div>
+              <div
+                className={
+                  item.id % 2 === 0
+                    ? "order-2 text-black text-lg md:text-3xl"
+                    : "order-1 text-black text-lg md:text-3xl"
+                }
+              >
+                <h1 className="my-5">{item.title}</h1>
+                <p className="text-lg">{item.des}</p>
+              </div>
+            </div>
+          </div>
         ))}
     </>
   );
