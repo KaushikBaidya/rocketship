e829192ae9f647200c3a2c3895a1321e9cda0861
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-10-31 17:16:38 +0600

    Update [id].js

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,29 +1,37 @@
 import Image from "next/image";
-import React from "react";
-import Service from "../../public/blog1.jpeg";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const SinglePage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const handledata = async () => {
+      const result = await fetch(`/api/blogs/${id}`);
+      const data = await result.json();
+      setData(data[0]);
+    };
+    handledata();
+  }, [id]);
+  console.log(data);
+
   return (
     <section class="text-gray-600 body-font">
       <div class="container mx-auto flex px-5 py-28 items-center justify-center flex-col">
         <Image
           class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
-          src={Service}
+          src={`https://drive.google.com/thumbnail?id=${data?.img}`}
           width={700}
           height={300}
         />
         <div class="text-left lg:w-2/3 w-full mt-5">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Microdosing synth tattooed vexillologist
+            {data?.title}
           </h1>
-          <p class="mb-8 leading-relaxed">
-            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
-            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
-            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
-            Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
-            booth af fingerstache pitchfork.
-          </p>
+          <p class="mb-8 leading-relaxed">{data?.description}</p>
         </div>
       </div>
     </section>
