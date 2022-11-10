c6748833feefef121ebeba138767c9d504e5004f
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-11-10 12:10:33 +0600

    Update index.js

--- pages/dashboard/achievement/index.js
+++ pages/dashboard/achievement/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
 import EditButton from '../../../components/dashboard/button/EditButton'
 import {
   ListCol,
